5f45167f477bfefc78f9bda218b39ae0aa783ca7
Saud <121959804+saudshah207@users.noreply.github.com>
2024-07-30 17:12:06 +0500

    Solve 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, timesToRepeat) {
+  let repeatedString = (timesToRepeat > -1) ? "" : "ERROR";
+  while (timesToRepeat > 0) {
+    repeatedString += string;
+    timesToRepeat--;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
