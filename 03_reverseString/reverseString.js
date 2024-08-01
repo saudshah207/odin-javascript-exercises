021463e650010033b3bc84a4028d99f9ed431455
Saud <121959804+saudshah207@users.noreply.github.com>
2024-08-01 18:54:00 +0500

    Solve 03_reverseString & add test for emojis

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let index = [...string].length - 1; index > -1; index--) {
+        reversedString += [...string][index];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
