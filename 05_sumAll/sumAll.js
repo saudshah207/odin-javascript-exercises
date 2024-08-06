8b45d4973e969a9f224f30b824a88677153fcf65
Saud <121959804+saudshah207@users.noreply.github.com>
2024-08-06 21:00:16 +0500

    Solve 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, lastNum) {
+  if (
+    firstNum > -1 &&
+    lastNum > -1 &&
+    Number.isInteger(firstNum) &&
+    Number.isInteger(lastNum)
+  ) {
+    let sum = 0;
+    if (firstNum > lastNum) {
+      [firstNum, lastNum] = [lastNum, firstNum];
+    }
+    while (lastNum >= firstNum) {
+      sum += lastNum;
+      lastNum--;
+    }
+    return sum;
+  } else return "ERROR";
 };
 
 // Do not edit below this line
