29a494eb424284be157ad049a8f5ad5790596274
saud <121959804+saudshah207@users.noreply.github.com>
2025-09-02 23:44:14 +0500

    Solve calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  const sum = numbers.reduce((sum, currentNumber) => sum += currentNumber, 0);
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (numbers) {
+  const product = numbers.reduce(
+    (product, currentNumber) => product * currentNumber,
+    1
+  );
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let factorial = 1;
+
+  if (number === 0) return factorial;
+
+  for (; number > 1; number--) factorial *= number;
+
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
