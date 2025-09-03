dc639fbe488122b08c8439b0734098286525281d
saud <121959804+saudshah207@users.noreply.github.com>
2025-09-03 12:34:12 +0500

    Solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase().split("");
 
+  function isNotPunctuationChar(char) {
+    const punctuationMarks = [" ", ",", "!", ".", ";"];
+    return !punctuationMarks.includes(char);
+  }
+
+  const reversed = string.filter(isNotPunctuationChar).reverse().join("");
+
+  string = string.filter(isNotPunctuationChar).join("");
+
+  return string === reversed;
 };
 
 // Do not edit below this line
