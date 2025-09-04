5c93b5ba70ecb2976bc434c8a7c8166f8109b209
saud <121959804+saudshah207@users.noreply.github.com>
2025-09-04 19:32:13 +0500

    Solve findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPersonUptilNow, currentPerson) => {
+    const oldestPersonDeathYear = oldestPersonUptilNow.yearOfDeath
+      ? oldestPersonUptilNow.yearOfDeath
+      : new Date().getFullYear();
 
+    const maxAge = oldestPersonDeathYear - oldestPersonUptilNow.yearOfBirth,
+      age = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+
+    if (age > maxAge) return currentPerson;
+    else return oldestPersonUptilNow;
+  });
 };
 
 // Do not edit below this line
