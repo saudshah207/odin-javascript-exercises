b9407e896bab402b03a0459855325db9daa1f6a6
saud <121959804+saudshah207@users.noreply.github.com>
2025-09-04 17:57:49 +0500

    Solve getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  /* map() creates and returns a new array, 
+    each item of which is the return value 
+    of the provided callback function */
+  return books.map(book => book.title);
 };
 
 // Do not edit below this line
