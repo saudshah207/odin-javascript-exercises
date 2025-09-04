f609739540803dfd9c4b441076e96230410911e2
saud <121959804+saudshah207@users.noreply.github.com>
2025-09-04 14:58:05 +0500

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  // our fibonacci sequence starts at 1, 1, 2, 3, 5,....
+  let secondPreceding = 1,
+    firstPreceding = secondPreceding,
+    nthFibonacci = 1;
 
+  n = +n;
+
+  if (n < 0) return "OOPS";
+
+  if (n === 0) return 0;
+
+  // F(n) = F(n - 1) + F(n - 2);
+  while (n > 2) {
+    secondPreceding = firstPreceding;
+    firstPreceding = nthFibonacci;
+
+    nthFibonacci = firstPreceding + secondPreceding;
+
+    n--;
+  }
+
+  return nthFibonacci;
 };
 
 // Do not edit below this line
